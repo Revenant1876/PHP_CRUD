d4a0aae1384bccaaa8b552117985544fc3a61af8
レイベン <159684957+Revenant1876@users.noreply.github.com>
2026-06-09 08:50:44 +0800

    Updated Code

--- auth.php
+++ auth.php
@@ -1,11 +1,10 @@
-<?php
+<?php
 session_start();
-
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
     header('Location: ' . (strpos($_SERVER['PHP_SELF'], '/CRUD/') ? '../' : '') . 'login.php');
     exit();
 }
-
+
 if (!isset($conn)) {
     include __DIR__ . '/config.php';
 }
